77a6022f04980e279b4e920cc9ea429d7ad14964
Francisco Javier Vadillo <32244264+FranVadillo@users.noreply.github.com>
2019-06-18 12:44:13 +0200

    Hasta el 70

--- js/pokedex.js
+++ js/pokedex.js
@@ -221,4 +221,202 @@ pokedex=[
             "description": "",
             "model": ""
         },
+        {
+            "id": 38,
+            "name": "Ninetales",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 39,
+            "name": "Jigglypuff",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 40,
+            "name": "Wigglytuff",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 41,
+            "name": "Zubat",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 42,
+            "name": "Golbat",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 43,
+            "name": "Oddish",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 44,
+            "name": "Gloom",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 45,
+            "name": "Vileplume",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 46,
+            "name": "Paras",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 47,
+            "name": "Parasect",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 48,
+            "name": "Venonat",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 49,
+            "name": "Venomoth",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 50,
+            "name": "Diglett",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 51,
+            "name": "Dugtrio",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 52,
+            "name": "Meowth",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 53,
+            "name": "Persian",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 54,
+            "name": "Psyduck",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 55,
+            "name": "Golduck",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 56,
+            "name": "Mankey",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 57,
+            "name": "Primeape",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 58,
+            "name": "Growlithe",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 59,
+            "name": "Arcanine",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 60,
+            "name": "Poliwag",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 61,
+            "name": "Poliwhirl",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 62,
+            "name": "Poliwrath",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 63,
+            "name": "Abra",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 64,
+            "name": "Kadabra",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 65,
+            "name": "Alakazam",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 66,
+            "name": "Machop",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 67,
+            "name": "Machoke",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 68,
+            "name": "Machamp",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 69,
+            "name": "Bellsprout",
+            "description": "",
+            "model": ""
+        },
+        {
+            "id": 70,
+            "name": "Weepinbell",
+            "description": "",
+            "model": ""
+        },
     ];
